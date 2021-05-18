fe9e7eebcd52e6f87324866e5297600e88e31c6a
Huy Pham <huypn6@fpt.edu.vn>
2021-05-18 21:24:28 +0700

    add code slide 1, 4

---
+++ SpringBootDemo/src/main/java/com/cuchuoi/Employee.java
@@ -0,0 +1,18 @@
+package com.cuchuoi;
+
+import org.springframework.stereotype.Component;
+
+@Component(value = "Employee")
+public class Employee {
+
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
---
+++ SpringBootDemo/src/main/java/com/cuchuoi/ManagementEmployee.java
@@ -0,0 +1,14 @@
+package com.cuchuoi;
+
+import java.util.List;
+
+public class ManagementEmployee {
+
+	List<Employee> employees;
+
+	public ManagementEmployee(List<Employee> employees) {
+		super();
+		this.employees = employees;
+	}
+	
+}
